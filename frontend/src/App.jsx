c9164bad088b046f7a5cda71fe119d274534ea5f
HassanMunene <awanzihassan@gmail.com>
2024-02-17 13:34:55 +0300

    added the route for the homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
-import Login from "./pages/login/Login";
-import SignUp from "./pages/signup/Signup";
+import { Routes, Route } from 'react-router-dom';
 import HomePage from "./pages/home/Home";
 
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <HomePage/>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+      </Routes>
     </div>
   )
 }
