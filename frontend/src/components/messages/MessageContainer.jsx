dbc7b697b6399f3380743def6ed0de13fe0f6ceb
HassanMunene <awanzihassan@gmail.com>
2024-02-18 17:27:04 +0300

    l

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,3 +1,4 @@
+import {useEffect} from 'react';
 import { useConversationStore } from '../../zustandStore/useConversationStore';
 import Messages from "./Messages";
 import MessageInput from "./MessageInput";
@@ -6,13 +7,19 @@ import { TiMessage } from "react-icons/ti";
 const MessageContainer = () => {
     const {selectedConversation, setSelectedConversation} = useConversationStore();
 
+    useEffect(() => {
+        //cleanup function when user logs out we want to discard the
+        // selectedConversation.
+        return () => setSelectedConversation(null);
+    }, [setSelectedConversation]);
+
     return (
         <div className="md:min-w-[450px] flex flex-col">
             {!selectedConversation ? <NoChatSelected/> : (
                 <>
                     <div className="bg-slate-500 px-4 py-2 mb-2">
                         <span className="label-text">To:</span>{" "}
-                        <span className="text-gray-900 font-bold">John Doe</span>
+                        <span className="text-gray-900 font-bold">{selectedConversation.fullname}</span>
                     </div>
                     <Messages/>
                     <MessageInput/>
