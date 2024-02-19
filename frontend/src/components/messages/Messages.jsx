3dd64eea23bac2a8b091fad999d40a57287d1d39
HassanMunene <awanzihassan@gmail.com>
2024-02-19 12:23:24 +0300

    complete the message section

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -8,6 +8,14 @@ const Messages = () => {
     return (
         <div className="px-4 flex-1 overflow-auto">
             {loading && [...Array(3)].map((_, index) => <LoadingMessageSkeleton key={index}/>)}
+            
+            {!loading && messages.length === 0 && (
+                <p className="text-center text-sm text-yellow-500">Send a message to start a conversation</p>
+            )}
+            
+            {!loading && messages.length > 0 && (
+                messages.map((message) => <SingleMessage key={message._id} message={message}/>)
+            )}
         </div>
     )
 }
--- frontend/src/components/messages/SingleMessage.jsx
+++ frontend/src/components/messages/SingleMessage.jsx
@@ -1,12 +1,24 @@
-const SingleMessage = () => {
+import {useAuthContext} from '../../context/AuthContext';
+import {useConversationStore} from '../../zustandStore/useConversationStore';
+
+const SingleMessage = ({message}) => {
+    const {authUser} = useAuthContext();
+    const {selectedConversation} = useConversationStore();
+
+    const isFromMe = message.senderId === authUser._id;
+    const chatClassName = isFromMe ? 'chat-end' : 'chat-start';
+    const profilePic = isFromMe ? authUser.profilePic : selectedConversation?.profilePic;
+    const bubbleBgColor = isFromMe ? 'bg-yellow-500' : '';
+
+
     return (
-        <div className="chat chat-end">
+        <div className={`chat ${chatClassName}`}>
             <div className="chat-image avatar">
                 <div className="w-10 rounded-full">
-                    <img alt="Tailwind CSS chat bubble component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                    <img alt="Tailwind CSS chat bubble component" src={profilePic} />
                 </div>
             </div>
-            <div className="chat-bubble text-black bg-yellow-400">Hi! what is up</div>
+            <div className={`chat-bubble text-black ${bubbleBgColor}`}>{message.message}</div>
             <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">12:42</div>
         </div>
     )
